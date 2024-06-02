6fbb25205d7bc7430d1608e8e427a48b8bc83c50
lizwachira <lizwanguwachira@gmail.com>
2024-06-02 15:10:50 +0300

    fib series done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1 
+    for _ in range(length):
+        print(a, end='')
+        a,b = b, a+b
+        print()
+print_fibonacci(9)
+    
